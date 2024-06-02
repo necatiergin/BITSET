a142203d38a35b76e3bba4a7564c7a4c42376ed4
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-02 07:39:47 +0300

    Update bitset_led_panel_01.cpp

--- bitset_led_panel_01.cpp
+++ bitset_led_panel_01.cpp
@@ -8,19 +8,19 @@ int main()
 	using namespace std;
 	using namespace chrono;
 
-	constexpr auto dur = 5ms;
+	constexpr auto dur = 150ms;
 	bitset<64> bs;
 
 	for (;;) {
 		bs = 1ull;
 		while (bs != 0) {
-			cout << bs << "\r";
+			cout << bs << '\r';
 			this_thread::sleep_for(dur);
 			bs <<= 1;
 		}
 		bs = ~(~0ull >> 1);
 		while (bs != 0) {
-			cout << bs << "\r";
+			cout << bs << '\r';
 			this_thread::sleep_for(dur);
 			bs >>= 1;
 		}
