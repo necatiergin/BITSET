2680e5e510d290574a7e0293cab2e85500eb81b6
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-02 07:44:41 +0300

    Update bitset_set_01.cpp

--- bitset_set_01.cpp
+++ bitset_set_01.cpp
@@ -6,15 +6,15 @@ int main()
 	using namespace std;
 
 	bitset<16> bs;
-	cout << bs << "\n";
+	cout << bs << '\n';
 	bs.set();
-	cout << bs << "\n";
+	cout << bs << '\n';
 	bs = 0;
-	cout << bs << "\n";
+	cout << bs << '\n';
 	bs.set(5);
-	cout << bs << "\n";
+	cout << bs << '\n';
 	bs.set(5, false);
-	cout << bs << "\n";
+	cout << bs << '\n';
 	bs.set(5, true);
-	cout << bs << "\n";
+	cout << bs << '\n';
 }
