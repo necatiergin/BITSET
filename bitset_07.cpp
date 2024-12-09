03ded181d9849ab1d30349d582fb71c0dbb457ca
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-09 09:50:07 +0300

    Update bitset_07.cpp

--- bitset_07.cpp
+++ bitset_07.cpp
@@ -3,15 +3,13 @@
 
 int main()
 {
-	using namespace std;
-
 	unsigned long uval;
-	cout << "bir tamsayi girin: ";
-	cin >> uval;
-	bitset<32> bs = uval;
+	std::cout << "bir tamsayi girin: ";
+	std::cin >> uval;
+	std::bitset<32> bs = uval;
 
-	cout << bs[10].operator bool() << '\n';
-	cout << bs[10] << '\n';
+	std::cout << bs[10].operator bool() << '\n';
+	std::cout << bs[10] << '\n';
 	bs[5] = bs[2];
 	bs[4].flip();
 	bs[7] = ~bs[2];
