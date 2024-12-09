e449eea83746c7fb07455bb1294a53751a1843df
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-09 10:01:11 +0300

    Update and rename bitset_led_panel_01.cpp to bitset_17.cpp

--- bitset_17.cpp
+++ bitset_17.cpp
@@ -5,23 +5,22 @@
 
 int main()
 {
-	using namespace std;
-	using namespace chrono;
+	using namespace std::literals;
 
 	constexpr auto dur = 150ms;
-	bitset<64> bs;
+	std::bitset<64> bs;
 
 	for (;;) {
 		bs = 1ull;
 		while (bs != 0) {
-			cout << bs << '\r';
-			this_thread::sleep_for(dur);
+			std::cout << bs << '\r';
+			std::this_thread::sleep_for(dur);
 			bs <<= 1;
 		}
 		bs = ~(~0ull >> 1);
 		while (bs != 0) {
-			cout << bs << '\r';
-			this_thread::sleep_for(dur);
+			std::cout << bs << '\r';
+			std::this_thread::sleep_for(dur);
 			bs >>= 1;
 		}
 	}
